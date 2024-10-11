7a5ebc263c57baccf6ef770747526fbd6c463504
Jwostik <70971752+Jwostik@users.noreply.github.com>
2024-10-11 15:02:51 +0000

    Add files via upload

---
+++ service.py
@@ -0,0 +1,12 @@
+from fastapi import FastAPI
+import uvicorn
+
+app = FastAPI()
+
+
+@app.get("/hello")
+async def root():
+    return {"body": "123"}
+
+if __name__ == '__main__':
+    uvicorn.run(app, port=8000, host='localhost')
